07371a0e1e0bb7595e75340ab622764a3378315d
Jackson Klingaman <jackson.klingaman22@gmail.com>
2025-01-22 12:23:18 -0600

    took me a little longer than it shouldve to see that you wanted me to put them in a const... passes all tests

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 2;
+const num3 = 3523;
+const num4 = 17;
+const mod = num3 % num4;
+const max = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
 
-// Write your code here
